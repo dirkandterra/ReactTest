036d507cd0a32bd3c4641361a11b9ac159b8097c
dirkandterra <dirkandterra@aol.com>
2019-07-26 09:02:46 -0500

    Branch

--- AgSyncConvert/AgSyncConvert/urls.py
+++ AgSyncConvert/AgSyncConvert/urls.py
@@ -19,5 +19,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainPage.urls'))
+    path('', include('mainPage.urls')),
+    path('', include('frontend.urls'))
+
 ]
